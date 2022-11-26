b84ee27cf4b580fe89a9c8473756b29981cec8e8
TieKotek <1603700364@qq.com>
2022-11-26 17:27:57 +0800

    finish code

--- README.md
+++ README.md
@@ -2,3 +2,31 @@
 
 # 大眼仔的任务罢了
 
+# Template1
+{{hello.DATA}}
+
+{{date.DATA}} 
+
+地区：{{region.DATA}} 
+
+天气：{{weather.DATA}} 
+
+气温：{{temp.DATA}} 
+
+风向：{{wind_dir.DATA}} 
+
+体感温度：{{feelsLike.DATA}}
+
+湿度：{{humidity.DATA}}
+
+{{message.DATA}}
+
+今天是我们认识的第{{meet_day.DATA}}天
+今天是我们在一起的第{{love_day.DATA}}天 
+
+{{birthday1.DATA}} 
+{{birthday2.DATA}} 
+
+
+{{note_en.DATA}} 
+{{note_ch.DATA}}
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime, date
 from zhdate import ZhDate
 import sys
 import os
+from config import config
  
  
 def get_color():
@@ -19,8 +20,8 @@ def get_access_token():
     app_id = config["app_id"]
     # appSecret
     app_secret = config["app_secret"]
-    post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
-                .format(app_id, app_secret))
+    post_url = f"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={app_id}&secret={app_secret}"
+                
     try:
         access_token = get(post_url).json()['access_token']
     except KeyError:
@@ -37,7 +38,7 @@ def get_weather(region):
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
     key = config["weather_key"]
-    region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, key)
+    region_url = f"https://geoapi.qweather.com/v2/city/lookup?location={region}&key={key}"
     response = get(region_url, headers=headers).json()
     if response["code"] == "404":
         print("推送消息失败，请检查地区名是否有误！")
@@ -58,7 +59,12 @@ def get_weather(region):
     temp = response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
     # 风向
     wind_dir = response["now"]["windDir"]
-    return weather, temp, wind_dir
+    # 体感
+    feels_like = response["now"]["feelsLike"]
+    # 湿度
+    humidity = response["now"]["humidity"]
+    time = response["updateTime"]
+    return weather, temp, wind_dir, feels_like, humidity, time
  
  
 def get_birthday(birthday, year, today):
@@ -115,8 +121,8 @@ def get_ciba():
     return note_ch, note_en
  
  
-def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
-    url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
+def send_message(to_user, access_token, region_name, weather, temp, wind_dir, feelslike, humidity, note_ch, note_en, hello_message):
+    url = f"https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={access_token}"
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
     month = localtime().tm_mon
@@ -127,9 +133,14 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
     love_year = int(config["love_date"].split("-")[0])
     love_month = int(config["love_date"].split("-")[1])
     love_day = int(config["love_date"].split("-")[2])
+    meet_year = int(config["meet_date"].split("-")[0])
+    meet_month = int(config["meet_date"].split("-")[1])
+    meet_day = int(config["meet_date"].split("-")[2])
     love_date = date(love_year, love_month, love_day)
+    meet_date = date(meet_year, meet_month, meet_day)
     # 获取在一起的日期差
     love_days = str(today.__sub__(love_date)).split(" ")[0]
+    meet_days = str(today.__sub__(meet_date)).split(" ")[0]
     # 获取所有生日数据
     birthdays = {}
     for k, v in config.items():
@@ -141,8 +152,12 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         "url": "http://weixin.qq.com/download",
         "topcolor": "#FF0000",
         "data": {
+            "hello": {
+                "value": hello_message,
+                "color": get_color()
+            },
             "date": {
-                "value": "{} {}".format(today, week),
+                "value": f"{today} {week}",
                 "color": get_color()
             },
             "region": {
@@ -161,8 +176,20 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
                 "value": wind_dir,
                 "color": get_color()
             },
+            "feelsLike": {
+                "value": feelslike,
+                "color": get_color()
+            },
+            "humidity": {
+                "value": humidity,
+                "color": get_color()
+            },
             "love_day": {
                 "value": love_days,
+                "color": get_color()
+            },
+            "meet_day": {
+                "value": meet_days,
                 "color": get_color()
             },
             "note_en": {
@@ -179,7 +206,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         # 获取距离下次生日的时间
         birth_day = get_birthday(value["birthday"], year, today)
         if birth_day == 0:
-            birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
+            birthday_data = "今天{}生日哦，{}生日快乐！！！！！！！！！！！！".format(value["name"], value["name"])
         else:
             birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
         # 将生日数据插入data
@@ -200,28 +227,27 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         print("推送消息成功")
     else:
         print(response)
- 
- 
+
+
+def is_morning(time):
+    cday = datetime.strptime(time, '%Y-%m-%dT%H:%M+08:00')
+    if cday.hour < 12: 
+        return True
+    return False
+
+
 if __name__ == "__main__":
-    try:
-        with open("config.txt", encoding="utf-8") as f:
-            config = eval(f.read())
-    except FileNotFoundError:
-        print("推送消息失败，请检查config.txt文件是否与程序位于同一路径")
-        os.system("pause")
-        sys.exit(1)
-    except SyntaxError:
-        print("推送消息失败，请检查配置文件格式是否正确")
-        os.system("pause")
-        sys.exit(1)
- 
     # 获取accessToken
     accessToken = get_access_token()
     # 接收的用户
     users = config["user"]
     # 传入地区获取天气信息
     region = config["region"]
-    weather, temp, wind_dir = get_weather(region)
+    weather, temp, wind_dir, feelslike, humidity, time = get_weather(region)
+    if is_morning(time):
+        hello_message = "猫猫早上好呀！！！有没有睡个好觉呢"
+    else:
+        hello_message = "猫猫下午好呀！！！狗狗想你噢，下午也要充满活力"
     note_ch = config["note_ch"]
     note_en = config["note_en"]
     if note_ch == "" and note_en == "":
@@ -229,5 +255,5 @@ if __name__ == "__main__":
         note_ch, note_en = get_ciba()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+        send_message(user, accessToken, region, weather, temp, wind_dir, feelslike, humidity, note_ch, note_en, hello_message)
     os.system("pause")
